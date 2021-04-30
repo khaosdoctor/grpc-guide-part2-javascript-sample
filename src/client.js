bc46eef9bd2591fde452b84ebdc69d7e487c77f0
Lucas Santos <lhs.santoss@gmail.com>
2021-04-29 23:05:42 -0300

    Build client

---
+++ src/client.js
@@ -0,0 +1,18 @@
+const grpc = require('@grpc/grpc-js')
+const protoLoader = require('@grpc/proto-loader')
+const path = require('path')
+
+const protoObject = protoLoader.loadSync(path.resolve(__dirname, '../proto/notes.proto'))
+const NotesDefinition = grpc.loadPackageDefinition(protoObject)
+
+const client = new NotesDefinition.NoteService('localhost:50051', grpc.credentials.createInsecure())
+
+client.list({}, (err, notes) => {
+  if (err) throw err
+  console.log(notes)
+})
+
+client.find({ id: '2' }, (err, { note }) => {
+  if (err) return console.error(err.details)
+  return console.log(note)
+})
